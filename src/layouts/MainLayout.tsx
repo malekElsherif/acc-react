f6140b2b066b6b7290ca869990eef02c67f7645f
malek <malek2062002@mail.com>
2026-09-14 21:19:53 +0300

    remove footer and nav from print file invoices

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,11 +5,19 @@ import Footer from "./Footer";
 const MainLayout = () => {
   return (
     <div className="min-h-screen bg-[#F1F0EC] flex flex-col">
-      <Navbar />
+      {/* إضافة print:hidden هنا لإخفاء النافبار عند الطباعة */}
+      <div className="print:hidden">
+        <Navbar />
+      </div>
+
       <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer />
+
+      {/* إضافة print:hidden هنا أيضاً لإخفاء الفوتر عند الطباعة */}
+      <div className="print:hidden">
+        <Footer />
+      </div>
     </div>
   );
 };
